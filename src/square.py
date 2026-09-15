00063f7dd43d410043afb517220550aac3740fd3
Thyago Mota <thyagomota@gmail.com>
2026-09-15 14:49:10 -0600

    updates on Square's constructor

--- src/square.py
+++ src/square.py
@@ -1,8 +1,10 @@
 class Square: 
 
-    # FIXME
     def __init__(self, side) -> None:
-        pass
+        if side <= 0:
+            self.side = 1
+        else:
+            self.side = side
 
     def area(self): 
         return self.side * self.side 
